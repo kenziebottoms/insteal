35ca20598d8b013a66c5abc226ed03ad9a01282a
Kenzie Bottoms <kenziebottoms@gmail.com>
2018-06-18 14:32:41 -0500

    testing bio and raw_feed in profile

--- test/insta.test.js
+++ test/insta.test.js
@@ -7,18 +7,16 @@ const {
   cleanFeed
 } = require('../insta');
 
-describe('insta.js', () => {
-  describe('getSharedData', () => {
-    it('should resolve into an object', () => {
-      getSharedData('kenziebottoms')
-        .then(data => {
-          assert.isObject(data);
-          assert.isDefined(data);
-        })
-        .catch(err => {
-          // auto fail
-          assert.isTrue(false);
-        });
-    });
+describe('getSharedData', () => {
+  it('should resolve into an object', () => {
+    getSharedData('kenziebottoms')
+      .then(data => {
+        assert.isObject(data);
+        assert.isDefined(data);
+      })
+      .catch(err => {
+        // auto fail
+        assert.isTrue(false);
+      });
   });
 });
--- test/profile.test.js
+++ test/profile.test.js
@@ -11,37 +11,90 @@ const {
   raw_feed
 } = require('../profile');
 
-describe('profile.js', () => {
-  describe('profile', () => {
-    it('should return an object', () => {
-      profile('kenziebottoms')
-        .then(data => {
-          assert.isObject(data);
-          assert.isDefined(data);
-        })
-        .catch(err => {
-          // auto fail
-          assert.isTrue(false);
-        });
-    });
-    it('should have specific properties', () => {
-      profile('kenziebottoms')
-        .then(data => {
-          assert.isTrue(data.hasOwnProperty('blocked_by_viewer'));
-          assert.isTrue(data.hasOwnProperty('followed_by_viewer'));
-          assert.isTrue(data.hasOwnProperty('has_channel'));
-          assert.isTrue(data.hasOwnProperty('edge_felix_combined_draft_uploads'));
-        })
-        .catch(err => {
-          // auto fail
-          assert.isTrue(false);
-        });
-    });
-    it('should have integers as followers', () => {
+describe('profile', () => {
+  it('should return an object', () => {
+    profile('kenziebottoms')
+      .then(data => {
+        assert.isObject(data);
+        assert.isDefined(data);
+      })
+      .catch(err => {
+        // auto fail
+        assert.isTrue(false);
+      });
+  });
+  it('should have specific properties', () => {
+    profile('kenziebottoms')
+      .then(data => {
+        assert.property(data, 'blocked_by_viewer');
+        assert.property(data, 'followed_by_viewer');
+        assert.property(data, 'has_channel');
+        assert.property(data, 'edge_felix_combined_draft_uploads');
+      })
+      .catch(err => {
+        // auto fail
+        assert.isTrue(false);
+      });
+  });
+  describe('followers and followed by properties', () => {
+    it('should be integers', () => {
       profile('kenziebottoms')
         .then(data => {
           assert.isNumber(data.edge_follow.count);
           assert.isNumber(data.edge_followed_by.count);
+        })
+        .catch(err => {
+          // auto fail
+          assert.isTrue(false);
+        });
+    });
+  });
+});
+
+describe('bio', () => {
+  it('should be a string', () => {
+    bio('whats')
+      .then(data => {
+        assert.isDefined(data);
+        assert.isString(data);
+      })
+      .catch(err => {
+        // auto fail
+        assert.isTrue(false);
+      });
+  });
+});
+
+describe('raw_feed', () => {
+  it('should resolve into an array of objects', () => {
+    raw_feed('kenziebottoms')
+      .then(data => {
+        assert.isDefined(data);
+        assert.isArray(data);
+        assert.isObject(data[0]);
+      })
+      .catch(err => {
+        // auto fail
+        assert.isTrue(false);
+      });
+  });
+  it('should have specific properties', () => {
+    raw_feed('kenziebottoms')
+      .then(data => {
+        assert.property(data[0], '__typename');
+        assert.property(data[0], 'display_resources');
+        assert.property(data[0], 'taken_at_timestamp');
+      })
+      .catch(err => {
+        // auto fail
+        assert.isTrue(false);
+      });
+  });
+  describe('shortcode property', () => {
+    it('should be a string', () => {
+      raw_feed('kenziebottoms')
+        .then(data => {
+          assert.isString(data[0].shortcode);
         })
         .catch(err => {
           // auto fail
